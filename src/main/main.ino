34c25fd77a8cc6475ba2e8f7fd4fd4d13ce88d96
Andrew <andrew.kevin.ellison@gmail.com>
2022-08-21 18:25:41 +0300

    Added the ISP code needed to write to a 32 kHz clocked ATTiny, switched main.ino to use clock 0 for interrupt timing, and updated to test code to try out the encoder.

--- src/main/main.ino
+++ src/main/main.ino
@@ -4,16 +4,11 @@
 
 #define MOTOR_PIN PA7
 #define ENCODER_PIN PA3
-#define TICK_PIN PA6
 
-#define ENCODER_TICKS_PER_REV 20  // set this to the number of 'ticks' the encoder makes per revolution
-#define DUTY_CYCLE 0.5  // A decimal for the duty cycle at the motor pin
-
-const int SECS_PER_TICK = 2;//3600 / (ENCODER_TICKS_PER_REV * 2);
-const unsigned int TIMER_COMPARE = (DUTY_CYCLE * 255);
-
-int sec_counter = SECS_PER_TICK; // Countdown until the next tick
-volatile bool tick = false;  // To notify of 1 second passing
+const uint8_t SECS_PER_TICK = 5;//3600 / ENCODER_TICKS_PER_REV;
+volatile uint8_t sec_counter = 0; // Countdown until the next tick
+volatile bool setState = false;
+bool state = false;
 
 //for setting register bits with AVR code
 //cbi and sbi are standard (AVR) methods for setting, or clearing, bits in PORT (and other) variables. 
@@ -24,48 +19,42 @@ volatile bool tick = false;  // To notify of 1 second passing
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
 
-
 void setup() {
-  cli(); //disable global interrupts
-
-  pinMode(MOTOR_PIN, OUTPUT);
   pinMode(ENCODER_PIN, INPUT_PULLUP);
-  pinMode(TICK_PIN, OUTPUT);
 
   sbi(GIMSK, PCIE0); // enable external pin change interrupts 0 - 7
-  PCMSK0 |= B00000100;  // enable PCINT3
+  PCMSK0 = B00001000; // enable PCINT3 / PA3
 
-  // Setting up clock 0 for PWM timing
-  TCCR0A = B00100011;   // Connect pin 0C0B (PA7) in fast PWM mode
-  TCCR0B = B00000001;   // No pre-scaling
-  OCR0B = TIMER_COMPARE;  // Count to TIMER_COMPARE before resetting
-
-  // Setting up clock 1 for main timing
-
-  TCCR1A = B01000000;   // Toggle pin OC1A (TICK_PIN) on timer compare interrupt A
-  TCCR1B = B00001101;   // Setting clear timer on compare with OCR1A, also scaling by 1024
-  OCR1A = 31;           // Output compare register, clock counts to this number
-  TIMSK1 = B00000010;   // Setting timer interrupt on compare A
-
+  //   Setting up clock 1 for main timing
+  TCCR0A = B00000010;   // Toggle pin OC0A (TICK_PIN) on timer compare interrupt A
+  TCCR0B = B00000101;   // Setting clear timer on compare with OCR1A, using time scaled by 1024
+  TIMSK0 = B00000010;   // Setting timer interrupt on compare A
+  OCR0A = 31;          // Output compare register, clock counts to this number
+  
   sei(); //enable global interrupts
+
+  pinMode(MOTOR_PIN, OUTPUT); // This is here because otherwise clock 1 doesn't work
+  digitalWrite(MOTOR_PIN, LOW);
 }
 
 void loop() {
-//  if (digitalRead(ENCODER_PIN) == tick) {
-////    pinMode(MOTOR_PIN, OUTPUT);
-//    digitalWrite(MOTOR_PIN, HIGH);
-//  } else {
-////    pinMode(MOTOR_PIN, INPUT);
-//    digitalWrite(MOTOR_PIN, LOW);
-//  } 
+  if (setState == !state) {
+    state = setState;
+    if (state)
+      digitalWrite(MOTOR_PIN, HIGH);
+    else
+      digitalWrite(MOTOR_PIN, LOW);
+  }
 }
 
-EMPTY_INTERRUPT(PCINT0_vect);  // Triggers on the encoder pin changing state
+ISR(PCINT0_vect) {  // Triggers on the encoder pin changing state
+  setState = !setState;
+}
 
-ISR(TIM1_COMPA_vect) {  // Triggers when 32 Hz timer reaches count of 32 (1Hz)
-  --sec_counter;
-  if(sec_counter == 0) {
-    sec_counter = SECS_PER_TICK;
-    tick = !tick;
+ISR(TIM0_COMPA_vect) {  // Triggers when 32 Hz timer reaches count of 32 (1Hz)
+  ++sec_counter;
+  if(sec_counter >= SECS_PER_TICK) {
+    setState = !setState;
+    sec_counter = 0;
   }
 }
